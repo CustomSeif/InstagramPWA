7c51bf3417a0d3ef659e89b3a9bf4ce1205baedc
Seif <msk-7@outlook.com>
2020-01-14 00:11:15 +0100

    Added explore posts with images

--- client/src/components/ExplorePosts/ExplorePost/ExplorePost.js
+++ client/src/components/ExplorePosts/ExplorePost/ExplorePost.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import "./ExplorePost.css"
 
-const ExplorePost = () => {
+const ExplorePost = ({ imageMediumURL, lazyURL }) => {
+    const [lazyImageLoaded, setLazyImageLoaded] = useState(false)
+    const [imageLoaded, setImageLoaded] = useState(false)
+
     return (
         <div className="ExplorePost">
+            <div className="ExplorePost__placeholder">
+            </div>
+
+            <div className={`ExplorePost__lazy ${lazyImageLoaded}`}>
+                <img src={lazyURL} alt="Random Lazy" onLoad={() => setLazyImageLoaded(true)} />
+            </div>
+
+            <div className={`ExplorePost__full ${imageLoaded}`}>
+                <img src={imageMediumURL} alt="Random" onLoad={() => setImageLoaded(true)} />
+            </div>
         </div>
     )
 }
--- client/src/components/ExplorePosts/ExplorePosts.js
+++ client/src/components/ExplorePosts/ExplorePosts.js
@@ -1,27 +1,66 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./ExplorePosts.css"
+import axios from "axios"
 import ExplorePost from "./ExplorePost/ExplorePost"
 
 const ExplorePosts = () => {
-    const explorePosts = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        axios.get("/posts")
+            .then(({ data }) => {
+                let count = 0
+                let posts = []
+                let post = []
+                for (let index in data) {
+                    if (count === 9) {
+                        posts.push(post)
+                        post = []
+                        count = 0
+                    }
+                    post.push(data[index])
+                    count = count + 1
+                }
+                posts.push(post)
+
+                setPosts(posts)
+            })
+    }, [])
 
     return (
         <div className="Explore__posts">
-            <div className="Explore__posts__grid Explore__posts__grid--right">
-                {
-                    explorePosts.map((_, index) => {
-                        return <ExplorePost key={index} />
-                    })
-                }
-            </div>
-
-            <div className="Explore__posts__grid Explore__posts__grid--left">
-                {
-                    explorePosts.map((_, index) => {
-                        return <ExplorePost key={index} />
-                    })
-                }
-            </div>
+            {
+                posts.map((nonad, index) => {
+                    if (index % 2 === 0) return (
+                        <div className="Explore__posts__grid Explore__posts__grid--right" key={index}>
+                            {
+                                nonad.map((post, index) => {
+                                    return (
+                                        <ExplorePost key={index}
+                                            imageMediumURL={post.image_medium_url}
+                                            lazyURL={post.lazy_url}
+                                        />
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                    else return (
+                        <div className="Explore__posts__grid Explore__posts__grid--left" key={index}>
+                            {
+                                nonad.map((post, index) => {
+                                    return (
+                                        <ExplorePost key={index}
+                                            imageMediumURL={post.image_medium_url}
+                                            lazyURL={post.lazy_url}
+                                        />
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
